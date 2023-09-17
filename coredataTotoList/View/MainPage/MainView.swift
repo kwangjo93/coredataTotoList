5c529dc1746f9745667408f6f012f6057644e9dd
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2023-09-17 18:01:10 +0900

    [DESIGN] : mainPage UI

--- coredataTotoList/View/MainPage/MainView.swift
+++ coredataTotoList/View/MainPage/MainView.swift
@@ -4,17 +4,93 @@
 //
 //  Created by 천광조 on 2023/09/17.
 //
-
 import UIKit
 
-class MainView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class MainView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        setupStackView()
+        setConstraints()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var mainImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let todoButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("할일 확인하기", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
+    let completedButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("완료한 일 보기", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
+    let profileButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("Profile 보기(ProfileDesignViewController)", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.numberOfLines = 0
+        button.titleLabel?.textAlignment = .center
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let sv = UIStackView()
+        sv.axis = .vertical
+        sv.distribution  = .fillEqually
+        sv.alignment = .fill
+        sv.spacing = 15
+        return sv
+    }()
+    
+    
+    
+    private func setupStackView() {
+        self.addSubview(mainImageView)
+        stackView.addArrangedSubview(todoButton)
+        stackView.addArrangedSubview(completedButton)
+        stackView.addArrangedSubview(profileButton)
+        // 뷰컨트롤러의 기본뷰 위에 스택뷰 올리기
+        self.addSubview(stackView)
+    }
+    
+    
+    // MARK: - 오토레이아웃
 
-}
+       private func setConstraints() {
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                mainImageView.heightAnchor.constraint(equalToConstant: 120),
+                mainImageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 60),
+                mainImageView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -40),
+                mainImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 40),
+                
+                stackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 40),
+                stackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -40),
+                stackView.topAnchor.constraint(equalTo: mainImageView.bottomAnchor, constant: 40),
+                stackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -300)
+       
+            ])
+        }
+    }
+
+
--- coredataTotoList/View/MainPage/MainViewController.swift
+++ coredataTotoList/View/MainPage/MainViewController.swift
@@ -6,24 +6,89 @@
 //
 
 import UIKit
+import SwiftUI
+
 
 class MainViewController: UIViewController {
 
+   private var mainView = MainView()
+   private var imageUrl = "https://spartacodingclub.kr/css/images/scc-og.jpg"
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        loadImage()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func loadView() {
+        self.view = self.mainView
     }
-    */
-
+    
+    
+    
+    
+    private func loadImage() {
+        guard let url = URL(string: imageUrl) else { return }
+            
+            DispatchQueue.global().async {
+                guard let data = try? Data(contentsOf: url) else { return }
+                DispatchQueue.main.async {
+                    self.mainView.mainImageView.image = UIImage(data: data)
+                }
+            }
+        }
+    
+    func todoButtonAction() {
+            mainView.todoButton.addTarget(self, action: #selector(todoButtonTapped), for: .touchUpInside)
+        }
+    
+    @objc func todoButtonTapped() {
+        
+    }
+    
+    func completedButtonAction() {
+        mainView.completedButton.addTarget(self, action: #selector(completedButtonTapped), for: .touchUpInside)
+        }
+    
+    @objc func completedButtonTapped() {
+        
+    }
+    
+    func profileButtoAction() {
+        mainView.profileButton.addTarget(self, action: #selector(profileButtoTapped), for: .touchUpInside)
+        }
+    
+    @objc func profileButtoTapped() {
+        
+    }
+    
+    
+    
+    struct MyViewController_PreViews: PreviewProvider {
+        static var previews: some View {
+            MainViewController().toPreview()
+        }
+    }
+    
 }
+
+
+// MARK: - UI Preview
+#if DEBUG
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+            let viewController: UIViewController
+
+            func makeUIViewController(context: Context) -> UIViewController {
+                return viewController
+            }
+
+            func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            }
+        }
+
+        func toPreview() -> some View {
+            Preview(viewController: self)
+        }
+}
+#endif
